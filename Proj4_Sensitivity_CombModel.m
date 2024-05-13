0ddc15cdb4e5e43544cae99f525b61a2f6515306
betsywoodward <125619319+betsywoodward@users.noreply.github.com>
2024-05-13 14:50:44 -0500

    Update Proj4_Sensitivity_CombModel.m

--- Proj4_Sensitivity_CombModel.m
+++ Proj4_Sensitivity_CombModel.m
@@ -5,9 +5,9 @@ lblmt = 100;
 ublmt = inf;
 
 % Model Alterations (Scenarios)
-addAce = 10000; % additional acetate
-blazercost=80;
-velvet = 19650;% * Based on optimal pants + shirt production
+addAce = 10000; % additional acetate (yards)
+blazercost=80; % additional cost of wool blazer production (dollars)
+velvet = 19650; % Based on optimal pants + shirt production
 
 % Scales for upper and lower bounds **INCREASING lbscale / DECREASING
 % ubscale REQUIRES CHANGES TO THE UPPER BOUND LIST FOR TAILORED SKIRTS.
@@ -21,17 +21,17 @@ matcost = [30 90 26 6.5 6.75 24.75 39-3*12 5 1.25 18-1.5*12 3.375 30];
 
 c = slprice-mnfcost-matcost;
 b = [28000 + addAce; 45000; 9000; 30000; velvet; 30000; 18000; 15000*ubscale; ublmt];
-A = [2 0   0 0   1.5 1.5 2 0 0	 0	 0   0;
-     3 0   0 0   0   2.5 0 0 0	 0	 0   0;
-     0 1.5 0 0   0   0   0 0 0	 0	 0   0.5;
-     0 0   0 0   2   0   0 0 0	 0	 1.5 0;
+A = [2 0   0 0   1.5 1.5 2 0 0	 0	0   0;
+     3 0   0 0   0   2.5 0 0 0	 0	0   0;
+     0 1.5 0 0   0   0   0 0 0	 0	0   0.5;
+     0 0   0 0   2   0   0 0 0	 0	1.5 0;
      0 0   0 0   0   0   3 0 0	 1.5 0   0;
-     0 0   0 0   0   0   0 2 0.5 0	 0   0;
-     0 0   2 0.5 0   0   0 0 0   0   0   0;
-     0 0   1 1   0   0   0 0 0   0   0   0;
-     0 0   0 0   0   0   0 1 1   0   0   0];
-lb = [4200 lblmt lblmt lblmt 2800 3000 lblmt lblmt lblmt lblmt lblmt lblmt]*lbscale;
-ub = [7000*2 4000 12000 15000 6000*2 5000*2 5500  ublmt ublmt 6000 ublmt ublmt]*ubscale;
+     0 0   0 0   0   0   0 2 0.5    0	0   0;
+     0 0   2 0.5 0   0   0 0 0      0   0   0;
+     0 0   1 1   0   0   0 0 0      0   0   0;
+     0 0   0 0   0   0   0 1 1      0   0   0];
+lb = [4200 lblmt lblmt lblmt 2800 3000 lblmt lblmt lblmt lblmt lblmt lblmt]*lbscale; %lower production bounds
+ub = [7000*2 4000 12000 15000 6000*2 5000*2 5500  ublmt ublmt 6000 ublmt ublmt]*ubscale; %upper production bounds
 options = optimoptions('linprog','Algorithm','dual-simplex');
 
 [xvec, fval, exitflag, output, lambda] = linprog(-c, A, b, [], [], lb, ub, options);
